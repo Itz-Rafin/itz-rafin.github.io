9c8dd3a8f03fe8df914ed0202b588cd9020d6d9f
Rafin Azim <23201227@uap-bd.edu>
2026-02-12 20:23:41 +0600

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu_icon');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+    strings: ['CSE Student', 'Backend Developer', 'Frontend Developer', 'Graphic Designer', 'Web Designer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+});
